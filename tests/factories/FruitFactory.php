59d88cfa3623f0a541e8fab6883876025c80093d
Franky So <frankyso.mail@gmail.com>
2019-12-28 18:34:54 +0700

    Finished development stage

---
+++ tests/factories/FruitFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Konnco\Transeloquent\Tests\factories;
+
+use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory as ModelFactory;
+use Konnco\Transeloquent\Tests\models\Fruit;
+
+/* @var ModelFactory $factory */
+$factory->define(Fruit::class, function (Faker $faker) {
+    return [
+    ];
+});
