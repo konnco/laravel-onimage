beca7bfa7441eacd2b56f35c6e220b8599e55317
Franky So <frankyso@users.noreply.github.com>
2020-08-23 13:33:11 +0000

    Apply fixes from StyleCI

--- src/models/Onimage.php
+++ src/models/Onimage.php
@@ -16,18 +16,18 @@ class Onimage extends Model
         return $this->morphTo();
     }
 
-    public function url($width=null, $height=null)
+    public function url($width = null, $height = null)
     {
-        if($width){
-            $width = "width=".$width;
+        if ($width) {
+            $width = 'width='.$width;
         }
 
-        if($height){
-            $height = "height=".$width;
+        if ($height) {
+            $height = 'height='.$width;
         }
 
         $query = [];
 
-        return url("/oc/media/{$this->name}?".implode("&",$query));
+        return url("/oc/media/{$this->name}?".implode('&', $query));
     }
 }
