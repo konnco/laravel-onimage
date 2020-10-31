d58d7ed11850dbf09ecf45c98017c232c6dd5fbc
frankyso <frankyso@users.noreply.github.com>
2020-10-31 18:58:02 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Konnco\Onimage\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Konnco\Onimage\OnimageServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
